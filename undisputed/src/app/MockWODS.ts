74630835c3582ccec7080aee7b087393e659d2f7
Brian <Brian@JOnessolutions.com>
2018-05-12 10:53:57 -0600

    Wod Component showing single wod

--- undisputed/src/app/MockWODS.ts
+++ undisputed/src/app/MockWODS.ts
@@ -7,14 +7,16 @@ export const MockWods: WODModel[] = [{'id': 1,
   'type': {'id': 1, 'name': 'WOD' }
   },
 'WODEntry': [{'id': 1,
-'name': 'Back Squats',
+'name': 'Partners split work',
 'order': 'A',
-'notes': 'Tempo',
-'items': [ {'id': 1, 'notes': '50 air squats'}, {'id': 2, 'notes': '5x5 75% Max'} ] },
+'notes': '4 rounds',
+'items': [ {'id': 1, 'notes': '20 Dead lifts 225/155'}, {'id': 2, 'notes': '40 Box jumps 24/20'}
+, {'id': 2, 'notes': '40 Push ups'} ] },
 {'id': 2,
-'name': 'EMOM - 10 minutes',
+'name': 'Rest 2 min',
 'order': 'B',
-'notes': 'Go Heavy',
-'items': [ {'id': 1, 'notes': '15 Pull-ups'}, {'id': 2, 'notes': '15 Burpees'} ]
+'notes': '4 rounds',
+'items': [ {'id': 1, 'notes': '400m run together'}, {'id': 2, 'notes': '40 RKS'}
+, {'id': 2, 'notes': '40 Jumping lunges'}]
 }]
 }];
