8f5241fa982e52f9121613fbcf213ba1955e1278
Eliot Ostling <eliotostling@Eliots-MacBook-Air.local>
2018-05-11 19:40:57 -0700

    Added bootstrap/nav component

--- undisputed/src/app/app.module.ts
+++ undisputed/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IMTComponent } from './imt/imt.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IMTComponent
+    IMTComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
